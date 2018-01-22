6a92a2187b981a89866a6441e5cd5b12dbf453bb
Gonzalo Marcote <gonzalomarcote@gmail.com>
2018-01-22 10:00:17 +0100

    Removed some data

--- ocp35.sh
+++ ocp35.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#subscription-manager register --username=rhn-support-gmarcote --password=1019goN\$4
-#subscription-manager attach --pool=8a85f9813cf493fe013d028b6cf75b5a
+#subscription-manager register --username= --password=
+#subscription-manager attach --pool=
 sudo subscription-manager repos --enable="rhel-7-server-rpms" --enable="rhel-7-server-extras-rpms" --enable="rhel-7-server-ose-3.5-rpms" --enable="rhel-7-fast-datapath-rpms"
 sudo yum -y update
 sudo yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct vim httpd-tools
